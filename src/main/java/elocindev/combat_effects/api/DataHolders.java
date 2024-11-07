b3da12c29bc7e3c976236f9121b0ab5a233625ca
nvb-uy <elocindev@lunapixel.gg>
2024-11-07 08:51:46 -0300

    feat(base config)

---
+++ src/main/java/elocindev/combat_effects/api/DataHolders.java
@@ -0,0 +1,43 @@
+package elocindev.combat_effects.api;
+
+import java.util.List;
+
+public class DataHolders {
+    public static class EffectHolder {
+        public static EffectHolder create(String effect_id, int duration, int amplifier, boolean show_particles) {
+            return new EffectHolder(effect_id, duration, amplifier, show_particles);
+        }
+
+        private EffectHolder(String effect_id, int duration, int amplifier, boolean show_particles) {
+            this.effect_id = effect_id;
+            this.duration = duration;
+            this.amplifier = amplifier;
+            this.show_particles = show_particles;
+        }
+
+        public String effect_id;
+        public int duration;
+        public int amplifier;
+        public boolean show_particles;
+    }
+
+    public static class EntityHolder {
+        public static EntityHolder create(String entity_regex, List<EffectHolder> effects_to_apply, int minimum_hp, boolean only_on_player_hit, boolean apply_to_attackers_instead) {
+            return new EntityHolder(entity_regex, effects_to_apply, minimum_hp, only_on_player_hit, apply_to_attackers_instead);
+        }
+        
+        private EntityHolder(String entity_regex, List<EffectHolder> effects_to_apply, int minimum_hp, boolean only_on_player_hit, boolean apply_to_attackers_instead) {
+            this.entity_regex = entity_regex;
+            this.effects_to_apply = effects_to_apply;
+            this.minimum_hp = minimum_hp;
+            this.only_on_player_hit = only_on_player_hit;
+            this.apply_to_attackers_instead = apply_to_attackers_instead;
+        }
+
+        public String entity_regex;
+        public List<EffectHolder> effects_to_apply;
+        public int minimum_hp = 0;
+        public boolean only_on_player_hit = false;
+        public boolean apply_to_attackers_instead = false;
+    }
+}
---
+++ src/main/java/elocindev/combat_effects/config/entries/MainConfig.java
@@ -0,0 +1,46 @@
+package elocindev.combat_effects.config.entries;
+
+import java.util.List;
+
+import elocindev.combat_effects.CombatEffects;
+import elocindev.combat_effects.api.DataHolders;
+import elocindev.necronomicon.api.config.v1.NecConfigAPI;
+import elocindev.necronomicon.config.Comment;
+import elocindev.necronomicon.config.NecConfig;
+
+public class MainConfig {
+    @NecConfig public static MainConfig INSTANCE;
+
+    public static String getFile() {
+        return NecConfigAPI.getFile(CombatEffects.MODID+".json5");
+    }
+
+    @Comment("-------------------------------------------------------------------------------------------------------------------------")
+	@Comment("                                             Combat Effects by ElocinDev")
+	@Comment("-------------------------------------------------------------------------------------------------------------------------")
+	@Comment(" ")
+	@Comment("Definitions:")
+	@Comment("  - entity_regex: A regex pattern to match an entity or multiple entities.")
+    @Comment("    * This is the entity the player engages in combat to apply the effects to the player.")
+    @Comment("  - effects_to_apply: A list of effects to apply, it can have a single one or multiple.")
+    @Comment("    * effect_id: The id of the effect to apply.")
+    @Comment("    * duration: The duration of the effect in ticks. (20 ticks = 1 second)")
+    @Comment("    * amplifier: The amplifier of the effect (Starts counting from 0!).")
+    @Comment("    * show_particles: If true, the particles of the effect will be shown.")
+    @Comment("  - minimum_hp: The minimum health the entity must have to apply the effects.")
+    @Comment("    * This is specially useful for bosses, you can set the minimum health to 200 to apply the effects to all entities.")
+    @Comment("  - only_on_player_hit: If true, the effects will only be applied if the player gets hit, and will not apply by just the player attacking the entity.")
+    @Comment("  - apply_to_attackers_instead: If true, the effects will be applied to the player's attackers instead of the player.")
+	@Comment(" ")
+	@Comment("-------------------------------------------------------------------------------------------------------------------------")
+    @Comment(" ")
+    @Comment("The default config adds makes the player get poison and wither when fighting example_entity.")
+    @Comment(" ")
+    public List<DataHolders.EntityHolder> entities = List.of(
+        DataHolders.EntityHolder.create("examplemod:example_entity", List.of(
+            DataHolders.EffectHolder.create("minecraft:poison", 100, 0, true),
+            DataHolders.EffectHolder.create("minecraft:wither", 100, 0, true)
+        ),
+        0, false, false)
+    );
+}
